edcc7869f88f6dfea127ecd9fc9a206a2e106fb6
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-20 09:51:22 +0900

    StateNotifier & StateNotifierProvider

---
+++ lib/provider/customer_level.dart
@@ -0,0 +1,27 @@
+import 'package:state_notifier/state_notifier.dart';
+import 'package:state_notifier_ex/provider/counter.dart';
+
+enum Level {
+  bronze,
+  silver,
+  gold,
+}
+
+class CustomerLevel extends StateNotifier<Level> with LocatorMixin {
+  CustomerLevel() : super(Level.bronze);
+
+  @override
+  void update(Locator watch) {
+    final currentCounter = watch<CounterState>().counter;
+
+    if (currentCounter >= 100) {
+      state = Level.gold;
+    } else if (currentCounter > 50 && currentCounter < 100) {
+      state = Level.silver;
+    } else {
+      state = Level.bronze;
+    }
+
+    super.update(watch);
+  }
+}
